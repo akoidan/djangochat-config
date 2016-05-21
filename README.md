f7afc421e371919013cab5e1473204d3e7241450
deathangel908 <123asd123>
2016-05-21 03:09:37 +0300

    added unicode for smileys

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 static files for https://github.com/Deathangel908/djangochat
+
+
+# SASS  questions
+   Can I minify inputs here?
+       select,
+        textarea,
+        div[contenteditable],
+        input[type=text], input[type=password],input [type=email], input[type=date]
+   How to move selector into mixing
+        @mixin inputs()
+            select,
+            textarea,
+            div[contenteditable],
+            input[type=text], input[type=password],input [type=email], input[type=date]
+   Is there a sass workouround withotu code duplicaiton to  deal with selector that contain invalid selector for firefox:
+        button,
+        input[type=file]::-webkit-file-upload-button {
+          background:red;
+        }
+      this selector won't work even for button in firefox
--- smileys/create_json_smileys_data.py
+++ smileys/create_json_smileys_data.py
@@ -4,15 +4,19 @@ from pprint import pprint
 with open('gif_base64.json') as bash_file:
     data_bash = json.load(bash_file)
 
-with open('static/smileys/info.json') as old_file:
+with open('info.json') as old_file:
     data_old = json.load(old_file)
 
 new_array = {}
+start_char = 13313
 for base_type in data_bash:
     new_array[base_type] = {}
     new = {v: k for k, v in data_old[base_type].items()}
     for smile_name in data_bash[base_type]:
-        new_array[base_type][new["{}.gif".format(smile_name)]] = data_bash[base_type][smile_name]
+        start_char += 1
+        new_array[base_type][chr(start_char)] = {}
+        new_array[base_type][chr(start_char)]['base64'] = data_bash[base_type][smile_name]
+        new_array[base_type][chr(start_char)]['text_alt'] = new["{}.gif".format(smile_name)]
 
-with open('static/smileys/smileys_data.json', 'w') as outfile:
+with open('smileys_data.json', 'w') as outfile:
     json.dump(new_array, outfile)
